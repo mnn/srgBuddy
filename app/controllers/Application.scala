66a58f40d9dba0059288fb0a5a6d66f4c700715c
moen <moen@366.hopto.org>
2014-01-21 19:41:18 +0100

    Implemented NoJS behaviour.

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -22,14 +22,14 @@ object Application extends Controller {
     implicit request =>
       if (request.queryString.contains("q")) {
         searchForm.bindFromRequest.fold(
-          errors => BadRequest(views.html.index(List.empty, errors, "", true)),
+          errors => BadRequest(views.html.index(List.empty, errors, "", true, request.uri)),
           q => {
             val searchResults = MappingDatabaseHolder.db.searchAny(q) //NameItem.searchPartials(q, MAX_RESULTS)
-            Ok(views.html.index(searchResults, searchForm.fill(q), NameItem.testListCreatedOn(), false, q))
+            Ok(views.html.index(searchResults, searchForm.fill(q), NameItem.testListCreatedOn(), false, request.uri, q))
           }
         )
       } else {
-        Ok(views.html.index(List.empty, searchForm, NameItem.testListCreatedOn(), true))
+        Ok(views.html.index(List.empty, searchForm, NameItem.testListCreatedOn(), true, request.uri))
       }
   }
 
